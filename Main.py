7b00b32c90c098a6cdee9aa28f236215d867bde5
wajedulislam <mdwajedulislam1999@gmail.com>
2026-04-09 15:37:49 +0900

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,8 @@
+#main.py
+from Sensor import TemperatureSensor, LightSensor
+
+temp = TemperatureSensor("Temp1")
+light = LightSensor("Light1")
+
+print(f"Temp: {temp.read()}")
+print(f"Light: {light.read()}")
---
+++ Sensor.py
@@ -0,0 +1,30 @@
+import random as r
+
+class Sensor:
+    """
+    Base sensor class.
+    """
+    def __init__(self, name: str)-> None:
+        self.name = name
+        
+    def read(self) -> float:
+        return 0.0 #to overwrite....
+    
+ #inheritance
+class TemperatureSensor(Sensor):
+    """
+    Simulated temp sensor.
+    """
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        
+    def read(self) -> float: #overwritten Sensor.read()
+        return round(r.uniform(20.0, 30.0), 2)
+    
+class LightSensor(Sensor):
+    """
+    Simulated light sensor.
+    """
+    def read(self) -> float: #overwritten
+        return round(r.uniform(0, 100),2)
+    
